a96d98e52fc2340c2bad6e0d5f12157fb0a23a1c
ramsai24 <jnrsai24@gmail.com>
2023-05-15 05:47:51 +0000

    into tof react js class1

---
+++ index.js
@@ -0,0 +1,7 @@
+let mutiElements = (
+  <div>
+    <h1 className="greeting">Hello {name}</h1>
+    <p>Good to see You </p>
+  </div>
+);
+ReactDOM.render(mutiElements, document.getElementById("root"));
